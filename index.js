074f4120bb7f7c8f3b4660751aaa50bbecf82a61
Abel Moro <abel.moro@conisel.com>
2025-03-20 09:28:28 +0100

    root get

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 5555;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    return res.status(200).json("BASIC NODE API")
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-});
+}); 
