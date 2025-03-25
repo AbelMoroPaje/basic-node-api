d39dc93805dacd323627aa40cf57b0af3780152f
Abel Moro <abel.moro@conisel.com>
2025-03-25 10:46:36 +0100

    CORS fix

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const port = process.env.PORT || 5555;
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 const relationships = require('./relationships')(sequelize);
 module.exports = {
     app,
